e2b9d50f765523e15ea54a0f52c7c635d97d2d02
Matthew Kewarth <mattkewarth@hotmail.com>
2018-02-04 18:24:50 -0500

    Lab 3 work

--- labs/03/microblog/js/api.js
+++ labs/03/microblog/js/api.js
@@ -15,33 +15,64 @@ var api = (function(){
     // return a message object
     module.addMessage = function(author, content){
         // store data here
-        var msg = {author: author, content: content}
-        localStorage.setItem(1, JSON.stringify(msg)); 
-        return msg
+        var msgs = JSON.parse(localStorage.getItem('todo'));
+        var msg = {id: msgs.next++, author: author, content: content, upvotes: 0, downvotes: 0};
+        msgs.items.push(msg);
+        localStorage.setItem('todo', JSON.stringify(msgs)); 
+        return msg;
     }
     
     // delete a message given its messageId
     // return a message object
     module.deleteMessage = function(messageId){
-        
+        var msgs = JSON.parse(localStorage.getItem('todo'));
+        var index = msgs.items.findIndex(function(item){
+            return item.id == messageId;
+        });
+        if (index == -1) return null;
+        var m = msgs.items[index];
+        msgs.items.splice(index, 1);
+        localStorage.setItem('todo', JSON.stringify(msgs));
+        return m;
     }
     
     // upvote a message given its messageId
     // return a message object
     module.upvoteMessage = function(messageId){
-        
+        var msgs = JSON.parse(localStorage.getItem('todo'));
+        var index = msgs.items.findIndex(function(item){
+            return item.id == messageId;
+        });
+        if (index == -1) return null;
+        msgs.items[index].upvotes++;
+        var m = msgs.items[index];
+        localStorage.setItem('todo', JSON.stringify(msgs));
+        return m;
     }
     
     // downvote a message given its messageId
     // return a message object
     module.downvoteMessage = function(messageId){
-        
+        var msgs = JSON.parse(localStorage.getItem('todo'));
+        var index = msgs.items.findIndex(function(item){
+            return item.id == messageId;
+        });
+        if (index == -1) return null;
+        msgs.items[index].downvotes++;
+        var m = msgs.items[index];
+        localStorage.setItem('todo', JSON.stringify(msgs));
+        return m;
     }
     
     // get 5 latest messages given an offset 
     // return an array of message objects
     module.getMessages = function(offset=0){
-        
+        var msgs = JSON.parse(localStorage.getItem('todo'));
+        m = [];
+        for(var i=offset; i < Math.min(offset + 5, msgs.items.length); i++){
+            m.push(msgs.items[i]);
+        }
+        return m;
     }
     
     return module;
--- labs/03/microblog/js/script.js
+++ labs/03/microblog/js/script.js
@@ -8,27 +8,31 @@ window.onload = (function(){
     
     function insertItem(item){
         var element = document.createElement('div');
-        element.className = "item";
-        element.innerHTML = '<div class="item_content">' + item.content + '</div>' +
+        element.className = "message";
+        element.innerHTML='<div class="message_user">' +
+            '<img class="message_picture" src="media/user.png" alt="' + item.author + '">' +
+            '<div class="message_username">' + item.author + '</div>' +
+            '</div>' +
+            '<div class="message_content">' + item.content + '</div>' +
+            '<div class="upvote-icon icon">' + item.upvotes + '</div>' +
+            '<div class="downvote-icon icon">' + item.downvotes + '</div>' +
             '<div class="delete-icon icon"></div>';
           element.querySelector('.delete-icon').addEventListener('click', function(){
-            api.deleteItem(item.id);
+            api.deleteMessage(item.id);
             element.parentNode.removeChild(element);
         }); 
-        document.querySelector('#items').prepend(element);
+        document.getElementById('#messages').appendChild(element);
     }
     
-    document.getElementById('create_message_form').addEventListener('submit', function(e){
+    document.getElementById("create_message_form").addEventListener('submit', function(e){
         // prevent from refreshing the page on submit
         e.preventDefault();
         // read form elements
         var todo = JSON.parse(localStorage.getItem('todo'));
-        var upvotes = 0;
-        var downvotes = 0;
         var username = document.getElementById("post_name").value;
         var content = document.getElementById("post_content").value;
         var index = todo.next;
-        var item = {id: todo.next++, content: [username, content]};
+        var item = api.addMessage(username, content);
         // clean form
         document.getElementById("create_message_form").reset();
         // create a new message element
@@ -39,40 +43,30 @@ window.onload = (function(){
             '<div class="message_username">' + username + '</div>' +
             '</div>' +
             '<div class="message_content">' + content + '</div>' +
-            '<div class="upvote-icon icon">' + upvotes + '</div>' +
-            '<div class="downvote-icon icon">' + downvotes + '</div>' +
+            '<div class="upvote-icon icon">' + 0 + '</div>' +
+            '<div class="downvote-icon icon">' + 0 + '</div>' +
             '<div class="delete-icon icon"></div>';
         // add this element to the document
         elmt.querySelector('.upvote-icon').addEventListener('click', function(){
-            upvotes++;
-            elmt.querySelector('.upvote-icon').innerHTML = upvotes;
+            var m = api.upvoteMessage(item.id);
+            elmt.querySelector('.downvote-icon').innerHTML = m.upvotes;
         });
         elmt.querySelector('.downvote-icon').addEventListener('click', function(){
-            downvotes++;
-            elmt.querySelector('.downvote-icon').innerHTML = upvotes;
+            var m = api.downvoteMessage(item.id);
+            elmt.querySelector('.downvote-icon').innerHTML = m.downvotes;
         });
         elmt.querySelector('.delete-icon').addEventListener('click', function(e){
             //Delete message
             elmt.parentNode.removeChild(elmt);
-            todo.items.splice(index, 1);
-            localStorage.setItem('todo', JSON.stringify(todo));
+            api.deleteMessage(index);
             
         });
-        
-        todo.items.push(item);
-        localStorage.setItem('todo', JSON.stringify(todo));
-        document.getElementById("messages").prepend(elmt);
+        document.getElementById("#messages").appendChild(elmt);
     });
     window.addEventListener('load', function(){
         var todo = JSON.parse(localStorage.getItem('todo'));
         todo.items.forEach(insertItem);
-        document.querySelector('#add_item').addEventListener('submit', function(e){
-            e.preventDefault();
-            var content = document.querySelector('#content_form').value;
-            document.querySelector('#add_item').reset();
-            var item = api.addItem(content);
-            insertItem(item);
-        });
+        
     });  
 }());
 
